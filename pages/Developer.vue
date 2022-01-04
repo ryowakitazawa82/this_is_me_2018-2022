ad8651e6cd7cc7d5a2335bff1b822aae42fbb6b7
ryowa.kitazawa <ryowa.kitazawa@gmail.com>
2022-01-04 18:16:54 +0900

    plannerページの頭の部分完成

--- pages/Developer.vue
+++ pages/Developer.vue
@@ -1,1 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
+</template>
 
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/Planner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="planner">
+    <div class="common-white-box">
+        <div class="common-white-box__inner">
+            <div class="common-w-title">
+                <div class="common-w-title__img-wrap">
+                <img src="/images/neko_musu.png" alt="ムスッとしたねこ">
+                </div>
+                <div class="common-w-title__text-wrap">
+                <p>プランナーの姿</p>
+                </div>
+            </div>
+            <div class="common-w-title__des">
+                <p>挑戦と挫折を繰り返しながら、プランナーとして日々奮闘してきました。</p>
+            </div>
+            <div class="common-w-h2">
+                <h2>ビジネスコンテスト</h2>
+            </div>
+            <div class="common-w-h2__des">
+                <p>大学2年の夏にビジネスコンテストに初挑戦。大学3年秋に挑戦したビジコンでは、準決勝・決勝大会に進出しました。</p>
+            </div>
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
