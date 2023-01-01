dad774c18a6f1d464ab696089f7bcbc9b7d3259b
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-01 23:08:37 +0900

    add: fisrtMovie

--- pages/FirstMovie.vue
+++ pages/FirstMovie.vue
@@ -1,21 +1,38 @@
 <template>
   <div clas="first-movie">
+    <div class="first-movie__mask">
+    </div>
     <div class="first-movie__main">
       <video
         src="video/thisisme.mp4"
-        autoplay
+        playsinline
+        muted
         class="first-movie__main--video"
+        id="video"
       ></video>
     </div>
 
     <div class="first-movie__next-button">
       <NuxtLink to="/">
-      <p class="first-movie__next-button--txt">SKIP</p>
+        <p class="first-movie__next-button--txt">SKIP</p>
       </NuxtLink>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    // const video = document.getElementById("video");
+    // video.addEventListener("canplay", () => {
+    //   video.classList.add("loaded");
+    // });
+
+    const button = document.getElementById("button");
+    button.addEventListener("click", function () {
+      video.muted = false;
+      video.play();
+    });
+  },
+};
 </script>
