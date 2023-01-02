3d0327312b6e235eee200d125ebfe11eff0765c1
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-02 10:46:12 +0900

    add: designerまで

--- pages/Designer.vue
+++ pages/Designer.vue
@@ -1,98 +1,116 @@
 <template>
   <div class="marketer">
     <div class="common-white-box">
-        <div class="common-white-box__inner">
-            <div class="common-w-title">
-                <div class="common-w-title__img-wrap">
-                    <img src="/images/neko_nomal.png" alt="普通の表情のねこ">
-                </div>
-                <div class="common-w-title__text-wrap">
-                <p>デザイナーの姿</p>
-                </div>
-            </div>
-            <div class="common-w-title__des">
-                <p>デザインは9割独学。初めはIllustratorがメインでしたが、最近はもっぱらXDを愛用。</p>
-            </div>
-
-            <div class="common-w-h2">
-                <h2>WEBデザイン</h2>
-            </div>
-            <div class="common-w-h2__des">
-                <p>個人で数件案件をこなした後に、ITベンチャーにデザイナーとしてジョインしました。</p>
-            </div>
-
-            <div class="common-w-content__wrap">
-                <div class="common-w-h3">
-                    <h3>コーポレートサイト</h3>
-                </div>
-                <div class="common-w-h3__img">
-                    <img src="/images/wd_01.png" alt="コーポレートサイト">
-                </div>
-            </div>
-
-            <div class="common-w-content__wrap">
-                <div class="common-w-h3">
-                    <h3>採用LP</h3>
-                </div>
-                <div class="common-w-h3__img">
-                    <img src="/images/wd_02.png" alt="採用LP">
-                </div>
-            </div>
-
-            <div class="common-w-content__wrap">
-                <div class="common-w-h3">
-                    <h3>広告LP</h3>
-                </div>
-                <div class="common-w-h3__img">
-                    <img src="/images/wd_03.png" alt="広告LP">
-                </div>
-            </div>
-
-
-            <div class="common-w-h2 plus-p-top">
-                <h2>グラフィックデザイン</h2>
-            </div>
-            <div class="common-w-h2__des">
-                <p>細かいパーツも基本的にAdobe Illustratorで自作していました。</p>
-            </div>
-
-            <div class="common-w-content__wrap">
-                <div class="common-w-h3">
-                    <h3>ポスティング広告</h3>
-                </div>
-                <div class="common-w-h3__img">
-                    <img src="/images/gd_01.png" alt="ポスティング広告01">
-                </div>
-                <div class="common-w-h3__img plus-p-top">
-                    <img src="/images/gd_01-2.png" alt="ポスティング広告02">
-                </div>
-            </div>
-
-            <div class="common-w-content__wrap">
-                <div class="common-w-h3">
-                    <h3>インスタバナー</h3>
-                </div>
-                <div class="common-w-h3__img">
-                    <img src="/images/gd_02.png" alt="インスタバナー">
-                </div>
-            </div>
-
-            <div class="common-w-content__wrap">
-                <div class="common-w-h3">
-                    <h3>広告バナー</h3>
-                </div>
-                <div class="common-w-h3__img">
-                    <img src="/images/gd_03.png" alt="広告バナー">
-                </div>
-            </div>
-
+      <div class="common-white-box__inner">
+        <div class="common-w-title">
+          <div class="common-w-title__img-wrap">
+            <img src="/images/neko_nomal.png" alt="普通の表情のねこ" />
+          </div>
+          <div class="common-w-title__text-wrap">
+            <p>デザイナーの姿</p>
+          </div>
         </div>
+        <div class="common-w-title__des">
+          <p>
+            デザインは9割独学。初めはIllustratorがメインでしたが、最近はもっぱらXDを愛用。
+          </p>
+        </div>
+
+        <div class="common-w-h2">
+          <h2>WEBデザイン</h2>
+        </div>
+        <div class="common-w-h2__des">
+          <p>
+            個人で数件案件をこなした後に、ITベンチャーにデザイナーとしてジョインしました。
+          </p>
+        </div>
+
+        <div class="common-w-content__flex">
+          <div class="common-w-content__wrap">
+            <div class="common-w-h3">
+              <h3>コーポレートサイト</h3>
+            </div>
+            <div class="common-w-h3__img">
+              <img
+                src="/images/wd_01.png"
+                alt="コーポレートサイト"
+                class="large-img"
+              />
+            </div>
+          </div>
+
+          <div class="common-w-content__wrap">
+            <div class="common-w-h3">
+              <h3>採用LP</h3>
+            </div>
+            <div class="common-w-h3__img">
+              <img src="/images/wd_02.png" alt="採用LP" class="large-img" />
+            </div>
+          </div>
+
+          <div class="common-w-content__wrap">
+            <div class="common-w-h3">
+              <h3>広告LP</h3>
+            </div>
+            <div class="common-w-h3__img">
+              <img src="/images/wd_03.png" alt="広告LP" class="large-img" />
+            </div>
+          </div>
+        </div>
+
+        <div class="common-w-h2 plus-p-top">
+          <h2>グラフィックデザイン</h2>
+        </div>
+        <div class="common-w-h2__des">
+          <p>細かいパーツも基本的にAdobe Illustratorで自作していました。</p>
+        </div>
+
+        <div class="common-w-content__wrap width-max">
+          <div class="common-w-h3">
+            <h3>ポスティング広告</h3>
+          </div>
+          <div class="common-w-content__flex">
+            <div class="common-w-h3__img">
+              <img
+                src="/images/gd_01.png"
+                alt="ポスティング広告01"
+                class="posting-img"
+              />
+            </div>
+            <div class="common-w-h3__img plus-p-top">
+              <img
+                src="/images/gd_01-2.png"
+                alt="ポスティング広告02"
+                class="posting-img"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="common-w-content__flex">
+          <div class="common-w-content__wrap">
+            <div class="common-w-h3">
+              <h3>インスタバナー</h3>
+            </div>
+            <div class="common-w-h3__img ">
+              <img src="/images/gd_02.png" alt="インスタバナー" class="grande-img"/>
+            </div>
+          </div>
+
+          <div class="common-w-content__wrap">
+            <div class="common-w-h3">
+              <h3>広告バナー</h3>
+            </div>
+            <div class="common-w-h3__img">
+              <img src="/images/gd_03.png" alt="広告バナー" class="grande-img"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
-
