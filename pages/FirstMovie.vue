177588e8f1d0f80749a1828a1d8d318aade85928
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-02 22:27:09 +0900

    fix

--- pages/FirstMovie.vue
+++ pages/FirstMovie.vue
@@ -5,7 +5,7 @@
         <p class="first-movie__mask--txt">
           ※音声が出ますので、音量にご注意ください。
         </p>
-        <div class="first-movie__mask--btn" id="ready-ok-button">OK!</div>
+        <div class="first-movie__mask--btn" @click="isOk">OK!</div>
       </div>
     </div>
     <div class="first-movie__main">
@@ -30,17 +30,18 @@
 export default {
   mounted() {
     const video = document.getElementById("video");
-    const movieMask = document.getElementById("movie-mask");
-    const readyOkButton = document.getElementById("ready-ok-button");
-    readyOkButton.addEventListener("click", function () {
-      movieMask.classList.add("ready-ok");
-      video.muted = false;
-      video.play();
-    });
     video.addEventListener("ended", function () {
       window.location.href = "/";
     });
   },
-  layout: 'normal',
+  methods: {
+    isOk() {
+      const movieMask = document.getElementById("movie-mask");
+      movieMask.classList.add("ready-ok");
+      video.muted = false;
+      video.play();
+    },
+  },
+  layout: "normal",
 };
 </script>
