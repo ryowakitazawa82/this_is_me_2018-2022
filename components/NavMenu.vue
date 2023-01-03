7867e06473623e564651135551c56a6862042409
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-03 17:31:53 +0900

    add: navMenu

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -7,22 +7,32 @@
     <nav class="nav__wrap animate-hamburger">
       <ul class="nav__lists">
         <li class="nav__list">
-          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="/"><p class="nav__list--txt">Home</p></NuxtLink>
         </li>
         <li class="nav__list">
-          <NuxtLink to="/Planner">Planner</NuxtLink>
+          <NuxtLink to="/Planner"
+            ><p class="nav__list--txt">Planner</p></NuxtLink
+          >
         </li>
         <li class="nav__list">
-          <NuxtLink to="/Marketer">Marketer</NuxtLink>
+          <NuxtLink to="/Marketer"
+            ><p class="nav__list--txt">Marketer</p></NuxtLink
+          >
         </li>
         <li class="nav__list">
-          <NuxtLink to="/Designer">Designer</NuxtLink>
+          <NuxtLink to="/Designer"
+            ><p class="nav__list--txt">Designer</p></NuxtLink
+          >
         </li>
         <li class="nav__list">
-          <NuxtLink to="/Developer">Developer</NuxtLink>
+          <NuxtLink to="/Developer"
+            ><p class="nav__list--txt">Developer</p></NuxtLink
+          >
         </li>
         <li class="nav__list">
-          <NuxtLink to="/Profile">Profile</NuxtLink>
+          <NuxtLink to="/Profile"
+            ><p class="nav__list--txt">Profile</p></NuxtLink
+          >
         </li>
       </ul>
     </nav>
