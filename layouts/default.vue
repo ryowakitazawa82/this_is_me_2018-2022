e3ebaf76384d250ccd0556da218bbf9477886322
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-03 16:03:13 +0900

    add: layouts

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Nuxt />
+    <div class="hamburger__btn" @click="toggleHamburger">
+      <div class="hamburger__icon animate-hamburger">
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="nav__wrap animate-hamburger"></nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleHamburger() {
+      const hamburgerAnimationElem =
+        document.querySelectorAll(".animate-hamburger");
+      hamburgerAnimationElem.forEach((i) => i.classList.toggle("is-active"));
+    },
+  },
+};
+</script>
