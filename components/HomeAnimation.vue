345e0d201f00b4f5d8df27790bc7a36dda3aa2bc
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-04 14:58:03 +0900

    add: 途中まで

---
+++ components/HomeAnimation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-animation__wrap"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.Animate();
+  },
+  methods: {
+    Animate() {},
+  },
+};
+</script>
