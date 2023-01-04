047fa28956d6a2498f3d24266da302cc873b8f2d
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-04 18:03:27 +0900

    add: firstanimation

--- components/HomeAnimation.vue
+++ components/HomeAnimation.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home-animation__wrap"></div>
+  <div class="home-animation__wrap" id="home-animation-mask">
+    <div class="home-animation">
+      <span>THIS IS ME IN 2018~2022</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,7 +12,12 @@ export default {
     this.Animate();
   },
   methods: {
-    Animate() {},
+    Animate() {
+      const homeAnimationMask = document.getElementById("home-animation-mask");
+      setTimeout(() => {
+        homeAnimationMask.classList.add("hidden");
+      }, 2600);
+    },
   },
 };
 </script>
